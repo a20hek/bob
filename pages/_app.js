3894926357eba7f2f29d6cb6baee7d11ea20f83a
abhishek <abbhishekk27@gmail.com>
2021-09-24 19:05:43 +0530

    supaaaa

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
-import { AuthProvider } from '../lib/auth';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../theme';
 import 'focus-visible/dist/focus-visible';
+import { UserContextProvider } from '../lib/UserContext';
+import { supabase } from '../utils/supabaseClient';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ChakraProvider theme={theme}>
-			{/* <AuthProvider> */}
-			<Component {...pageProps} />
-			{/* </AuthProvider> */}
+			<UserContextProvider supabaseClient={supabase}>
+				<Component {...pageProps} />
+			</UserContextProvider>
 		</ChakraProvider>
 	);
 }
