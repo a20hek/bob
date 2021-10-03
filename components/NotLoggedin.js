4d8e25e1c50102c18b98cbaa84882b15fa20aa00
abhishek <abbhishekk27@gmail.com>
2021-10-03 08:30:14 +0530

    loading component added

--- components/NotLoggedin.js
+++ components/NotLoggedin.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Router from 'next/router';
 
 import { Button, Heading, Text, Flex, Center } from '@chakra-ui/react';
 
 export default function NotLoggedin() {
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 3000);
+	}, []);
 	return (
-		<Flex direction='column' align='center' mt='10%'>
-			<Center>
-				<Heading>You&apos;re not logged in</Heading>
-			</Center>
-			<Center>
-				<Text>Click here to redirect to the login page</Text>
-			</Center>
-			<br />
-			<Center>
-				<Button
-					bgColor='#0EB500'
-					_hover={{ bg: '#13DA01' }}
-					textColor='#ffffff'
-					onClick={() => Router.push('/login')}>
-					Login
-				</Button>
-			</Center>
-		</Flex>
+		<>
+			{loading ? null : (
+				<Flex direction='column' align='center' mt='10%'>
+					<Center>
+						<Heading>You&apos;re not logged in</Heading>
+					</Center>
+					<Center>
+						<Text>Click here to redirect to the login page</Text>
+					</Center>
+					<br />
+					<Center>
+						<Button
+							bgColor='#0EB500'
+							_hover={{ bg: '#13DA01' }}
+							textColor='#ffffff'
+							onClick={() => Router.push('/login')}>
+							Login
+						</Button>
+					</Center>
+				</Flex>
+			)}
+		</>
 	);
 }
